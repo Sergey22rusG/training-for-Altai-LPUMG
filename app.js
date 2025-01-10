0b22e053b6e5300d372848b90891c3e98adf8c33
Sergey22rusG <22rus1983@gmail.com>
2025-01-10 14:00:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
-    .then(function(registration) {
-      console.log('Service Worker зарегистрирован:', registration);
-    })
-    .catch(function(error) {
-      console.log('Ошибка регистрации Service Worker:', error);
+// Получаем все элементы аккордеона
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+// Добавляем обработчик событий для каждого заголовка
+accordionItems.forEach(item => {
+  const header = item.querySelector('.accordion-header');
+  header.addEventListener('click', () => {
+    // Закрываем все открытые элементы
+    accordionItems.forEach(otherItem => {
+      if (otherItem !== item) {
+        otherItem.classList.remove('active');
+      }
     });
-}
+
+    // Переключаем состояние текущего элемента
+    item.classList.toggle('active');
+  });
+});
