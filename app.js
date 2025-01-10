15887ea28fb86c8492f57584ceaeed92aefa259d
Sergey22rusG <22rus1983@gmail.com>
2025-01-10 13:11:47 +0700

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
-    .then(function(registration) {
-      console.log('Service Worker зарегистрирован:', registration);
-    })
-    .catch(function(error) {
-      console.log('Ошибка регистрации Service Worker:', error);
-    });
-}
